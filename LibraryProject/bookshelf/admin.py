39fe269e54ccf62d0b1a57a5d4a3c321d3469c06
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-10-31 16:57:50 +0100

    Update admin.py

--- LibraryProject/bookshelf/admin.py
+++ LibraryProject/bookshelf/admin.py
@@ -6,3 +6,4 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
+
