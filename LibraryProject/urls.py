4357e9c0683a135db97a43748768b3158e8a19b4
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-10-31 19:54:09 +0100

    Delete LibraryProject/urls.py

--- LibraryProject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', include('bookshelf.urls')),  # si tu crées des urls pour l'app
-]
