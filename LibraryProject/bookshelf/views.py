377bf7734b129c281692efb446ea4d35554dad30
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-10-31 19:48:54 +0100

    Delete LibraryProject/bookshelf/views.py

--- LibraryProject/bookshelf/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Book
-
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
