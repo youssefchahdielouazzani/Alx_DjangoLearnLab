dd01cb52de1caa9924ac1ebc90a8475fd9de999b
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-10-31 19:48:39 +0100

    Delete LibraryProject/bookshelf/tests.py

--- LibraryProject/bookshelf/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from .models import Book
-
-class BookModelTest(TestCase):
-    def test_book_creation(self):
-        book = Book.objects.create(title='Test Book', author='Author', publication_year=2025)
-        self.assertEqual(book.title, 'Test Book')
