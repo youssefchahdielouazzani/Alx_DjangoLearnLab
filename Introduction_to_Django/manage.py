45cbf3914d98ba7ebbfcf15697120b7427b3dc93
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-10-31 16:35:51 +0100

    Create manage.py

---
+++ Introduction_to_Django/manage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+def main():
+    """Exécute les commandes de gestion Django depuis la ligne de commande."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Impossible d'importer Django. Assurez-vous qu'il est installé et "
+            "disponible sur votre variable d'environnement PYTHONPATH. "
+            "Avez-vous activé un environnement virtuel ?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
