9ad2efb3dcd9fa6667ce27a819056b5774464cec
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-07 11:01:03 +0100

    Update urls.py

--- django-models/ django_models/urls.py
+++ django-models/ django_models/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
+
 ]
 
