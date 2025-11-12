417feee70475836d2ec15cf9bdc6b8a25520419c
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-12 15:39:47 +0100

    Update wsgi.py

--- LibraryProject/wsgi.py
+++ LibraryProject/wsgi.py
@@ -5,6 +5,7 @@ WSGI config for LibraryProject project.
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
 
 application = get_wsgi_application()
