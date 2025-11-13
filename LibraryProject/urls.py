97315d23846765ecd0ea3bb4f36acf2afd67d4cd
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-13 11:44:21 +0100

    Update urls.py

--- LibraryProject/urls.py
+++ LibraryProject/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('admin/', admin.site.urls),
+    
+    # 🔽 On inclut ici les URLs de l'application relationship_app
+    path('', include('relationship_app.urls')),
 ]
+
 
 
