bd8b6ff06aaf57e0377bfd67e0578d8c696b4e56
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-13 13:14:45 +0100

    Create urls.py

---
+++ LibraryProject/LibraryProject/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # 🔽 On inclut ici les URLs de l'application relationship_app
+    path('', include('relationship_app.urls')),
+]
