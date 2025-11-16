36425eb1db419ab0f24d615e1241893c6d40a227
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-16 09:49:47 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,1 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("L'utilisateur doit avoir un email")
+
+        email = self.normalize_email(email)
+
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Le superuser doit avoir is_staff=True")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Le superuser doit avoir is_superuser=True")
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
