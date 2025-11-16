112f88976f177173c6091be4b87312959a48805c
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-16 10:23:48 +0100

    Delete LibraryProject/relationship_app/urls.py

--- LibraryProject/relationship_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    
-    # 🔽 On inclut ici les URLs de l'application relationship_app
-    path('', include('relationship_app.urls')),
-]
