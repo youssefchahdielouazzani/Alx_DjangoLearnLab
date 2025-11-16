74f23546e05933f2fcb00948dec9007e29be32d1
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-16 11:04:23 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,51 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from accounts.models import CustomUser
 
-# --------------------------
-# Custom User Manager
-# --------------------------
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email=None, password=None, **extra_fields):
-        if not username:
-            raise ValueError('The Username must be set')
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email=None, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(username, email, password, **extra_fields)
-
-# --------------------------
-# Custom User Model
-# --------------------------
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profiles/', null=True, blank=True)
-
-    objects = CustomUserManager()
-
-# --------------------------
-# Article Model avec permissions personnalisées
-# --------------------------
-class Article(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField()
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     class Meta:
         permissions = [
-            ("can_view", "Can view article"),
-            ("can_create", "Can create article"),
-            ("can_edit", "Can edit article"),
-            ("can_delete", "Can delete article"),
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
         ]
 
     def __str__(self):
