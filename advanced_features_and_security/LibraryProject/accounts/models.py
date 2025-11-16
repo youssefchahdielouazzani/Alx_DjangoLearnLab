157b2ad5366fd52730dc705e64f47094fb447a14
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-16 12:43:18 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/accounts/models.py
+++ advanced_features_and_security/LibraryProject/accounts/models.py
@@ -3,14 +3,8 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# ------------------------------
-# Custom User Manager
-# ------------------------------
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, date_of_birth=None, password=None, **extra_fields):
-        """
-        Crée et retourne un utilisateur standard.
-        """
         if not email:
             raise ValueError('L’adresse email doit être renseignée')
         email = self.normalize_email(email)
@@ -25,27 +19,12 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, username, email, date_of_birth=None, password=None, **extra_fields):
-        """
-        Crée et retourne un superutilisateur.
-        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Le superutilisateur doit avoir is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Le superutilisateur doit avoir is_superuser=True.')
-
         return self.create_user(username, email, date_of_birth, password, **extra_fields)
 
 
-# ------------------------------
-# Custom User Model
-# ------------------------------
 class CustomUser(AbstractUser):
-    """
-    Modèle utilisateur personnalisé avec date de naissance et photo de profil.
-    """
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
@@ -53,3 +32,4 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
