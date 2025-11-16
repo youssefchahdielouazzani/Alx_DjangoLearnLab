7439ced8075e2da81a81558ec11d5d28dc3316b8
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-16 12:54:21 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/accounts/models.py
+++ advanced_features_and_security/LibraryProject/accounts/models.py
@@ -1,5 +1,3 @@
-# advanced_features_and_security/accounts/models.py
-
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
@@ -8,12 +6,7 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError('L’adresse email doit être renseignée')
         email = self.normalize_email(email)
-        user = self.model(
-            username=username,
-            email=email,
-            date_of_birth=date_of_birth,
-            **extra_fields
-        )
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -23,13 +16,9 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
         return self.create_user(username, email, date_of_birth, password, **extra_fields)
 
-
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
     objects = CustomUserManager()
-
     def __str__(self):
         return self.username
-
