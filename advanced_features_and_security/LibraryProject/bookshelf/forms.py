cb5fbc3284be42159a286eca632c033b1ca49a7a
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-16 11:35:41 +0100

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from .models import Book
 
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
 
     def clean_title(self):
         title = self.cleaned_data.get('title')
-        # Exemple simple de nettoyage : retirer les balises HTML pour éviter XSS
         return forms.utils.strip_tags(title)
 
     def clean_author(self):
         author = self.cleaned_data.get('author')
         return forms.utils.strip_tags(author)
+
