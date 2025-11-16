ff8a530827eb490aba840d2c3e9e6407e3d981db
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-16 12:56:08 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,20 +1,17 @@
-# advanced_features_and_security/bookshelf/models.py
-
 from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     published_date = models.DateField()
-
     def __str__(self):
         return self.title
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(blank=True, null=True)
-
     def __str__(self):
         return self.name
+
 
 
 
