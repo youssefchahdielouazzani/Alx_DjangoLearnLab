c29483d07529090278a06035606daa5fcd9d926b
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-16 08:41:30 +0100

    Update models.py

--- advanced_features_and_security/accounts/models.py
+++ advanced_features_and_security/accounts/models.py
@@ -1,28 +1,28 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class CustomUserManager(BaseUserManager):
-
     def create_user(self, username, email, password=None, **extra_fields):
         if not email:
-            raise ValueError("Users must have an email address")
+            raise ValueError("L'utilisateur doit avoir un email")
 
         email = self.normalize_email(email)
+
         user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-
     def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Le superuser doit avoir is_staff=True")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Le superuser doit avoir is_superuser=True")
 
         return self.create_user(username, email, password, **extra_fields)
-
 
 
 class CustomUser(AbstractUser):
@@ -33,3 +33,4 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
