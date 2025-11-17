c78ca00f95a225a411d2e8743ec49cd682808220
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-17 18:08:24 +0100

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
-from .models import Book
 from .forms import ExampleForm
 
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
-
-def search_books(request):
-    query = request.GET.get('q', '')
-    books = Book.objects.filter(title__icontains=query)
-    return render(request, 'bookshelf/book_list.html', {'books': books})
-
-def add_book(request):
-    if request.method == 'POST':
-        form = ExampleForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = ExampleForm()
-    return render(request, 'bookshelf/form_example.html', {'form': form})
-
+def form_example(request):
+    form = ExampleForm()
+    return render(request, 'form_example.html', {'form': form})
