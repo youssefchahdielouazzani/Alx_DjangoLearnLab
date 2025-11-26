303ef9e1f0220ec6570efcd48793a89300d31f55
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-26 18:31:01 +0100

    Create views.py

---
+++ advanced-api-project/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+
+
+# Liste + création de livres
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# Liste + création d'auteurs (avec affichage des livres imbriqués)
+class AuthorListCreateView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
