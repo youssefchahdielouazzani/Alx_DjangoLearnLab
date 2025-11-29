37dd0b34f9918547e2bc2a84f5d459039901170c
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-11-29 11:35:48 +0100

    Update urls.py

--- advanced-api-project/ api/urls.py
+++ advanced-api-project/ api/urls.py
@@ -1,20 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from .views import (
-    BookListView,
-    BookDetailView,
-    BookCreateView,
-    BookUpdateView,
-    BookDeleteView,
-)
+from api.views import BookListView
 
 urlpatterns = [
-    # List and Detail Views
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-
-    # CRUD Views
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('admin/', admin.site.urls),
+    path('api/books/', BookListView.as_view(), name='book-list'),
 ]
 
