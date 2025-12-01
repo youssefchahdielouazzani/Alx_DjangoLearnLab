462ea2251adfa94d00d79eeb3aab6e6acf7ada4a
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-01 11:24:36 +0100

    Update apps.py

--- advanced-api-project/ api/apps.py
+++ advanced-api-project/ api/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    name = 'api'  # doit correspondre exactement au nom du dossier de l'app
+
