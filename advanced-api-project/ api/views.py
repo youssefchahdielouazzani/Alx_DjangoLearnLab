b674e14e75d9acbfcf3e6e8232ab91f309945299
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-01 17:32:20 +0100

    Update views.py

--- advanced-api-project/ api/views.py
+++ advanced-api-project/ api/views.py
@@ -3,21 +3,18 @@ from .models import Book
 from .serializers import BookSerializer
 
 
-# LIST ALL BOOKS (GET) – Public
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
 
 
-# RETRIEVE ONE BOOK BY ID (GET) – Public
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
 
 
-# CREATE A NEW BOOK (POST) – Authenticated
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
@@ -27,7 +24,6 @@ class BookCreateView(generics.CreateAPIView):
         serializer.save()
 
 
-# UPDATE A BOOK (PUT/PATCH) – Authenticated
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
@@ -37,9 +33,7 @@ class BookUpdateView(generics.UpdateAPIView):
         serializer.save()
 
 
-# DELETE A BOOK (DELETE) – Authenticated
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
-
