c42b611b9837a59ba044ce85fbe07856196f51e6
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-01 16:40:50 +0100

    Delete advanced_features_and_security/LibraryProject/bookshelf/views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .forms import ExampleForm
-
-def form_example(request):
-    form = ExampleForm()
-    return render(request, 'form_example.html', {'form': form})
