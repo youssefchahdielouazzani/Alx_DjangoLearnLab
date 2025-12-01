1fb9529e11ef7a6d67079f765716fb9944b882a2
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-01 11:13:48 +0100

    Update settings.py

--- advanced-api-project/advanced-api-project/settings.py
+++ advanced-api-project/advanced-api-project/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',  # <-- CORRECT: pas 'books'
+    'api',  
 ]
 
 # ----------------------------
