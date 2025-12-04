a87f41512a9d95ee3bde1a656c15dec77db2178b
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-04 17:20:13 +0100

    Delete advanced-api-project/ api/apps.py

--- advanced-api-project/ api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class ApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'  # doit correspondre exactement au nom du dossier de l'app
-
