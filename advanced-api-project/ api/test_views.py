55e64dca339d23f15fae830a3283388e692683ae
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-10 08:59:08 +0100

    Update test_views.py

--- advanced-api-project/ api/test_views.py
+++ advanced-api-project/ api/test_views.py
@@ -1,15 +1,14 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
-from django.urls import reverse
 from .models import Book
 
-class BookViewTests(TestCase):
+class BookTests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
         self.book = Book.objects.create(title="Test Book", author="Author")
 
     def test_book_list(self):
-        url = reverse('book-list')
-        response = self.client.get(url)
+        response = self.client.get('/api/books/')
         self.assertEqual(response.status_code, 200)
+
