d685395747a267e272616b34401443a9d8968e85
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-10 07:05:12 +0100

    Delete advanced-api-project/api/models.py

--- advanced-api-project/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    publication_year = models.IntegerField()
-
-    def __str__(self):
-        return self.title
