660efa6e0d35b5d5beb8c731f3a20607fa9cdb82
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-10 17:54:52 +0100

    Update models.py

--- advanced-api-project/ api/models.py
+++ advanced-api-project/ api/models.py
@@ -3,6 +3,11 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    publication_year = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    published_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
 
 
