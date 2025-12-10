51645342ac6f5a113eacd5490e687c5fd78d2150
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-10 18:24:27 +0100

    Update urls.py

--- advanced-api-project/ api/urls.py
+++ advanced-api-project/ api/urls.py
@@ -3,9 +3,10 @@ from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 
 router = DefaultRouter()
-router.register('books', BookViewSet, basename='books')
+router.register(r'books', BookViewSet, basename='book')
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
 
