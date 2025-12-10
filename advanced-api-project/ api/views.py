f1c4de50513dd3f920d49749cf165a2bfadcb9d6
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-10 07:29:13 +0100

    Update views.py

--- advanced-api-project/ api/views.py
+++ advanced-api-project/ api/views.py
@@ -1,1 +1,17 @@
+from rest_framework import viewsets, filters
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # Filtering, search & ordering for ALX requirements
+    filter_backends = [
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
 
