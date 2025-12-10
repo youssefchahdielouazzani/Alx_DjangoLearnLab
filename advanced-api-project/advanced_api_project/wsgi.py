09aa3df47bbf5647b118fb1aea2e4b3ce7707fdd
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-10 07:22:27 +0100

    Update wsgi.py

--- advanced-api-project/advanced_api_project/wsgi.py
+++ advanced-api-project/advanced_api_project/wsgi.py
@@ -1,1 +1,13 @@
+"""
+WSGI config for advanced_api_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advanced_api_project.settings')
+
+application = get_wsgi_application()
 
