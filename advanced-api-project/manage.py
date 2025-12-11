dc045844d9639811e63d5634fc7dd616f9021b35
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 14:13:52 +0100

    Update manage.py

--- advanced-api-project/manage.py
+++ advanced-api-project/manage.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def main():
+    """Exécute les commandes Django via la ligne de commande."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advanced_api_project.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Impossible d'importer Django. Assurez-vous qu'il est installé et "
+            "disponible sur votre PYTHONPATH."
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
 
 
 
