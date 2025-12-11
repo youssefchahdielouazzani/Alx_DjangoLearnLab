390cee1b9a0dba893a06f8ba83c5a360f18f2ecd
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 11:46:19 +0100

    Update tests.py

--- advanced-api-project/api/tests.py
+++ advanced-api-project/api/tests.py
@@ -1,1 +1,30 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from .models import Book
+
+class BookAPITestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.book = Book.objects.create(title='Book 1', author='Author 1', published_date='2025-01-01', created_by=self.user)
+
+    def test_list_books(self):
+        url = reverse('book-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_book_authenticated(self):
+        self.client.login(username='testuser', password='password123')
+        url = reverse('book-create')
+        data = {'title': 'New Book', 'author': 'Author 2', 'published_date': '2025-12-01'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_book_unauthenticated(self):
+        url = reverse('book-create')
+        data = {'title': 'New Book', 'author': 'Author 2', 'published_date': '2025-12-01'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
