66f9de85360c32e59e31c341508c77ad02cb558b
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 10:47:31 +0100

    Remove BookList and BookViewSet classes
    Removed BookList and BookViewSet classes from views.py.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,15 +1,4 @@
-from rest_framework.generics import ListAPIView
-from rest_framework import viewsets
-from .models import Book
-from .serializers import BookSerializer
 
-class BookList(ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
 
 
 
