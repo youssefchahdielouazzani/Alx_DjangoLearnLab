6e77f222239ed3dbee34dcc2071bb9a281a2f71e
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 03:08:53 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,2 +1,10 @@
+# api/views.py
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
