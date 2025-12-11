a99646df866ff121101fc1760c37eb165bb6f90a
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 10:57:31 +0100

    Add Book model with fields and owner relationship

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,3 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
 
 
 
