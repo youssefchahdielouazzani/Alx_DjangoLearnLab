781c9393a6d104107cb022ebadaa9a5610ad2449
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 13:46:13 +0100

    Update permissions.py

--- advanced-api-project/api/permissions.py
+++ advanced-api-project/api/permissions.py
@@ -1,12 +1,2 @@
-from rest_framework import permissions
 
-class IsAdminOrReadOnly(permissions.BasePermission):
-    """
-    Permission personnalisée : seul l'admin peut créer/modifier/supprimer.
-    Lecture accessible à tous.
-    """
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user and request.user.is_staff
 
