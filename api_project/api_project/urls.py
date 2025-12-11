cef6f951c6321822278a55ee77c0d63a2e0ca719
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 02:52:15 +0100

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,13 +1,2 @@
-"""api_project URL Configuration
 
-The `urlpatterns` list routes URLs to views.
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # On inclura plus tard les URLs de l'app api
-    # path('api/', include('api.urls')),
-]
 
