14d1b405e16af543213b6253a1e5621094ff8be4
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 02:56:37 +0100

    Update tests.py

--- api_project/api/tests.py
+++ api_project/api/tests.py
@@ -1,12 +1,3 @@
-from django.test import TestCase
-from .models import Book
 
-class BookModelTest(TestCase):
-    def setUp(self):
-        Book.objects.create(title="Test Book", author="Author A")
-
-    def test_book_creation(self):
-        book = Book.objects.get(title="Test Book")
-        self.assertEqual(book.author, "Author A")
 
 
