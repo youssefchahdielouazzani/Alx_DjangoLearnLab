1f65996a2da56dd694c227378d8c0d0b2a162600
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 09:36:27 +0100

    Remove BookAdmin from Django admin
    Removed BookAdmin class and its registration from admin.

--- api_project/api/admin.py
+++ api_project/api/admin.py
@@ -1,9 +1,1 @@
-# api/admin.py
-from django.contrib import admin
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'published_date')
-
 
