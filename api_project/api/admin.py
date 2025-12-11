e1e276227759daf8648370ba48e85fbc34349689
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 10:56:00 +0100

    Add Book model to Django admin interface

--- api_project/api/admin.py
+++ api_project/api/admin.py
@@ -1,2 +1,9 @@
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'published_date')
+    search_fields = ('title', 'author')
 
 
