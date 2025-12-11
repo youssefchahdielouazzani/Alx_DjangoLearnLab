dad46f045a1e4f3df8dd044aecd91188273cc688
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 13:05:43 +0100

    Delete advanced-api-project/urls.py

--- advanced-api-project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),     # Admin Django
-    path('api/', include('api.urls')),   # Inclusion des URLs de l'application API
-]
