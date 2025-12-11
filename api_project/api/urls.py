84dcd612c2b2edbe3316692080b0e6c9dc05867c
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 02:57:00 +0100

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,2 @@
-from django.urls import path
-from . import views
 
-urlpatterns = [
-    # On ajoutera les endpoints API plus tard
-    # path('books/', views.BookList.as_view(), name='book-list'),
-]
 
