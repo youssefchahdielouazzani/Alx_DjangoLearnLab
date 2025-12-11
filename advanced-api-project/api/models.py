7d137268b2d27735accb17a6859ebe34785924bb
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 11:42:12 +0100

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,1 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
 
