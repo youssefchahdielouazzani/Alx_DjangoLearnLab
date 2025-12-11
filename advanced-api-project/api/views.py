b1b292912774edc708047cc8463cb953143b90bc
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 14:07:42 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,1 +1,11 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Book
+from .serializers import BookSerializer
+from .permissions import IsAdminOrReadOnly
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]  # Lecture ouverte, modifications réservées aux admin
 
