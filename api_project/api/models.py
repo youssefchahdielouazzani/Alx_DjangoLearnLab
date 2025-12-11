c31543f8716abc4c56486ba5c2be5a2b9de3fa64
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 03:06:54 +0100

    Update models.py

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,2 +1,12 @@
+# api/models.py
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
 
 
