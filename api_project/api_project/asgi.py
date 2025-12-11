712e1ab2e50b1215bc46c9336e2db282d2e3af91
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 03:01:28 +0100

    Update asgi.py

--- api_project/api_project/asgi.py
+++ api_project/api_project/asgi.py
@@ -1,2 +1,9 @@
+# api_project/asgi.py
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_project.settings')
+
+application = get_asgi_application()
 
 
