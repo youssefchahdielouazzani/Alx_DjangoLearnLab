20de7885444b1058c997df4692b19bbcf8d7f78f
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 03:05:31 +0100

    Update admin.py

--- api_project/api/admin.py
+++ api_project/api/admin.py
@@ -1,2 +1,9 @@
+# api/admin.py
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'published_date')
 
 
