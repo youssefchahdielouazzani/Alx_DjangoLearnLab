1d9e6f1a12b2e19a08845febab341e794a0b12b1
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 02:57:27 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,2 @@
-from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
 
-# Vue DRF pour lister les livres et créer un livre
-class BookListCreateView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
 
