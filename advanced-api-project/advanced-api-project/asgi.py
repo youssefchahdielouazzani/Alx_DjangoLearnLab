fa98afc53f0b8c913a75b54afcb87b735655da41
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 13:01:52 +0100

    Create asgi.py

---
+++ advanced-api-project/advanced-api-project/asgi.py
@@ -0,0 +1,12 @@
+"""
+ASGI config for pi_project project.
+
+It exposes the ASGI callable as a module-level variable named `application`.
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pi_project.settings')
+
+application = get_asgi_application()
