d6a27275187baf0ca75b96cdc1660c72770fb713
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 14:06:52 +0100

    Update permissions.py

--- advanced-api-project/api/permissions.py
+++ advanced-api-project/api/permissions.py
@@ -1,2 +1,12 @@
+from rest_framework.permissions import BasePermission
+
+class IsAdminOrReadOnly(BasePermission):
+    """
+    Les utilisateurs non-admin peuvent uniquement lire.
+    """
+    def has_permission(self, request, view):
+        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+            return True
+        return request.user and request.user.is_staff
 
 
