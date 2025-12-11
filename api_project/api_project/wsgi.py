8bd9dff665768d07282d054c9a7c43b8ee068ae0
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 09:34:23 +0100

    Clean up wsgi.py by removing comments and whitespace
    Removed unnecessary comments and empty lines from wsgi.py.

--- api_project/api_project/wsgi.py
+++ api_project/api_project/wsgi.py
@@ -1,9 +1,3 @@
-# api_project/wsgi.py
-import os
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_project.settings')
-
-application = get_wsgi_application()
 
 
