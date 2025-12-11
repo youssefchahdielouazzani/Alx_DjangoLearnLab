dc846ad96e2fc05bd26d9e596a80b677ca93ad2e
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 02:59:11 +0100

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -1,1 +1,93 @@
+# api_project/settings.py
+from pathlib import Path
+
+# Chemin de base du projet
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Clé secrète (à changer pour un vrai projet)
+SECRET_KEY = 'django-insecure-change_this_for_production'
+
+# Mode debug activé pour le développement
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Applications installées
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    'rest_framework',  # Django REST Framework
+    'api',             # Notre app pour les livres
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'api_project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'api_project.wsgi.application'
+
+# Base de données SQLite par défaut
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Mots de passe
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Langue et fuseau horaire
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+# Fichiers statiques
+STATIC_URL = 'static/'
+
+# Valeur par défaut pour les champs AutoField
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
