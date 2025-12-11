694b419d647b4ed597b13f0dbf96e0321566c439
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 09:38:18 +0100

    Remove BookSerializer from serializers.py

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,8 +1,1 @@
-# api/serializers.py
-from rest_framework import serializers
-from .models import Book
 
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'
