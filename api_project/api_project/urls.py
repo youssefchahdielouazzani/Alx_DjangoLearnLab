25cc6d9cd305962a29adfede675e1cc4041ca699
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 10:52:44 +0100

    Add token authentication endpoint to URLs

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,3 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
+]
 
 
 
