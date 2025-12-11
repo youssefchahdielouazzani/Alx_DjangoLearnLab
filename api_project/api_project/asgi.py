70c6b8d7d0c1da255fe00e0a3ea8dc9662c54855
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 09:32:13 +0100

    Clean up asgi.py by removing comments and imports
    Removed unnecessary comments and imports from asgi.py.

--- api_project/api_project/asgi.py
+++ api_project/api_project/asgi.py
@@ -1,9 +1,3 @@
-# api_project/asgi.py
-import os
-from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_project.settings')
-
-application = get_asgi_application()
 
 
