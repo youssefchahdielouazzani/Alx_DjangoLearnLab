2a241ffe541d1112dab6d89fae6ffb21fe09934d
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 10:59:26 +0100

    Set up API routing for BookViewSet

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,3 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
 
 
 
