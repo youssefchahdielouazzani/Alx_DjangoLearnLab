c1ffaeb797bd772f56cd486d09c9a38f26c0a466
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 13:44:35 +0100

    Update admin.py

--- advanced-api-project/api/admin.py
+++ advanced-api-project/api/admin.py
@@ -1,9 +1,2 @@
-from django.contrib import admin
-from .models import Book
 
-# Enregistrer le modèle Book pour l'administration Django
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'published_date', 'created_by')
-    search_fields = ('title', 'author')
 
