4a1b23c9093f05bc69fcf59c2b70c651b4b4074c
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 09:38:55 +0100

    Delete BookList view from views.py
    Removed the BookList view implementation.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,3 @@
-# api/views.py
-from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
 
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
 
 
