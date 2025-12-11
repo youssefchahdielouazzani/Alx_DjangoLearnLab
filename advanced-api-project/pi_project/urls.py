8910dcb523cb2e00d0a59f0cdb1c41caad5cf6e4
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 12:49:12 +0100

    Update urls.py

--- advanced-api-project/pi_project/urls.py
+++ advanced-api-project/pi_project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Inclusion des URLs de l'application API
+    path('admin/', admin.site.urls),     # Admin Django
+    path('api/', include('api.urls')),   # Inclusion des URLs de l'application API
 ]
 
