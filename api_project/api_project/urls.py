736652e34936ed8c61c31cf2b526476485af70ea
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 02:59:57 +0100

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,2 +1,10 @@
+# api_project/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Inclut les URLs de notre app api
+]
 
 
