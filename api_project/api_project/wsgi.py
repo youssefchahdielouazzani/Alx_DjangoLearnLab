ce6f19f58401c7c87b7557e80524b0df5e6e86f9
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 03:00:47 +0100

    Update wsgi.py

--- api_project/api_project/wsgi.py
+++ api_project/api_project/wsgi.py
@@ -1,2 +1,9 @@
+# api_project/wsgi.py
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_project.settings')
+
+application = get_wsgi_application()
 
 
