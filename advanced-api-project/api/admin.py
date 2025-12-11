cbe94aa98fdcacd0015bf716641d6a2d06164ea2
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-11 11:39:29 +0100

    Add Book model to Django admin
    Register the Book model with the Django admin site.

--- advanced-api-project/api/admin.py
+++ advanced-api-project/api/admin.py
@@ -1,1 +1,9 @@
+from django.contrib import admin
+from .models import Book
+
+# Enregistrer le modèle Book pour l'administration Django
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'published_date', 'created_by')
+    search_fields = ('title', 'author')
 
