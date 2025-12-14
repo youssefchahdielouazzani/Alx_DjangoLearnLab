b6e8e7f102a3328e63c4d210ae6a046ed765335d
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 17:12:23 +0100

    Update tests.py

--- social_media_api/notifications/tests.py
+++ social_media_api/notifications/tests.py
@@ -1,1 +1,19 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Notification
+
+class NotificationModelTest(TestCase):
+    def test_notification_creation(self):
+        user1 = User.objects.create_user(username='actor', password='pass')
+        user2 = User.objects.create_user(username='recipient', password='pass')
+
+        notification = Notification.objects.create(
+            recipient=user2,
+            actor=user1,
+            verb='liked your post'
+        )
+
+        self.assertEqual(notification.recipient, user2)
+        self.assertEqual(notification.actor, user1)
+        self.assertEqual(notification.verb, 'liked your post')
 
