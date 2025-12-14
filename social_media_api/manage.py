8fcf12c737f7ab5dacf02ea5b53d70f29e2acc35
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 15:05:47 +0100

    Update manage.py

--- social_media_api/manage.py
+++ social_media_api/manage.py
@@ -6,14 +6,9 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable?"
-        ) from exc
+    except ImportError:
+        raise
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
-
-
