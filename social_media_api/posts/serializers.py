95f48b190803558372367ea46f35248f49bc4261
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 11:03:29 +0100

    Update serializers.py

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,1 +1,17 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
 
