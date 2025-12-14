18c5a7e16d3bc2736bbeecce974d33eef6b64ffb
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 17:40:29 +0100

    Update tests.py

--- social_media_api/posts/tests.py
+++ social_media_api/posts/tests.py
@@ -1,13 +1,2 @@
-from django.test import TestCase
-from django.contrib.auth.models import User
-from .models import Post, Like
 
-class LikeModelTest(TestCase):
-    def test_like_creation(self):
-        user = User.objects.create_user(username='user1', password='pass')
-        post = Post.objects.create(author=user, content='Test post')
-        like = Like.objects.create(user=user, post=post)
-
-        self.assertEqual(like.user, user)
-        self.assertEqual(like.post, post)
 
