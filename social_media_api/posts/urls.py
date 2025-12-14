28150daa3ae2d84db39f983354eee582f4e25f2c
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 14:25:05 +0100

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<int:pk>/like/', views.like_post, name='like-post'),
+    path('<int:pk>/unlike/', views.unlike_post, name='unlike-post'),
+]
 
 
 
