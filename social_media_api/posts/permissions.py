3b9bafc74f9e9801702aa7bb3c26e26e86b97ffd
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 11:43:08 +0100

    Update permissions.py

--- social_media_api/posts/permissions.py
+++ social_media_api/posts/permissions.py
@@ -1,8 +1,2 @@
-from rest_framework.permissions import BasePermission
 
-class IsOwnerOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
-            return True
-        return obj.author == request.user
 
