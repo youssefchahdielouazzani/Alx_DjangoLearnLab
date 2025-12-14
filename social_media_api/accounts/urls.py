a270bee92781cefd0b2bbcc84bfadd3cd481b862
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 14:07:10 +0100

    Delete social_media_api/accounts/urls.py

--- social_media_api/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import FollowUserView, UnfollowUserView
-
-urlpatterns = [
-    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
-]
-
-
