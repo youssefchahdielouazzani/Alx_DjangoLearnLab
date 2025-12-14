f33dcd5c6978eebf3767849a3361ef99f6ec8bfc
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 14:42:07 +0100

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,23 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Post {self.id} by {self.author.username}"
-
 class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liked_posts")
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ('post', 'user')  # Empêche les doublons
-
-    def __str__(self):
-        return f"{self.user.username} likes Post {self.post.id}"
-
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
