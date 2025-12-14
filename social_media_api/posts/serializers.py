82b4efd052ec6259a28fb8f2d4b00c3d43bfbfa6
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 17:53:59 +0100

    Update serializers.py

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,2 +1,15 @@
+from rest_framework import serializers
+from .models import Post, Like
+
+class PostSerializer(serializers.ModelSerializer):
+    likes_count = serializers.IntegerField(source='likes.count', read_only=True)
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'content', 'created_at', 'likes_count']
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['id', 'post', 'user', 'created_at']
 
 
