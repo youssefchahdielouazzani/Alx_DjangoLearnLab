6a52f93f77a0ff8cd54fbceb50e94cd284c2c4f6
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 16:56:35 +0100

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
