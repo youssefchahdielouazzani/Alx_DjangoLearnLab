804a88eaaebea7156d788bf2a988f7d1c887c5d4
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 12:05:50 +0100

    Update manage.py

--- social_media_api/manage.py
+++ social_media_api/manage.py
@@ -7,12 +7,9 @@ def main():
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed?"
-        ) from exc
+        raise ImportError("Couldn't import Django") from exc
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
-
 
