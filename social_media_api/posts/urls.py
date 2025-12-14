fa227879b01d6ca40e5be4e91d29227b9902a777
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 11:05:21 +0100

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,1 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
+
+urlpatterns = router.urls
 
