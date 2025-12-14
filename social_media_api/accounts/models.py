3825d04715ddc22f59fb4d25ab6bf7d68b28bdc9
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 10:30:21 +0100

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
-    profile_picture = models.URLField(blank=True)
+
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/',
+        blank=True,
+        null=True
+    )
+
     followers = models.ManyToManyField(
         'self',
         symmetrical=False,
@@ -12,4 +19,5 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
 
