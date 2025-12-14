28707805aab07debb8fa46d1dbf83e72a16cac15
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 14:18:27 +0100

    Update urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -1,3 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('posts/', include('posts.urls')),
+    path('notifications/', include('notifications.urls')),
+]
 
 
 
