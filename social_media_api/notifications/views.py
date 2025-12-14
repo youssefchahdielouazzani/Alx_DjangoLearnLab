29d174bfed692f2bcae69be000c2322b3eb6fec3
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 17:43:58 +0100

    Update views.py

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,7 +1,2 @@
-from django.http import JsonResponse
-from .models import Notification
 
-def notification_list(request):
-    notifications = Notification.objects.all()
-    return JsonResponse({'notifications': notifications.count()})
 
