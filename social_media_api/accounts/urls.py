e80589b72d56657dd33a8b011b4b43fc91d7b8c2
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 10:48:01 +0100

    Delete social_media_api/accounts/urls.py

--- social_media_api/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('profile/', ProfileView.as_view()),
-]
-
