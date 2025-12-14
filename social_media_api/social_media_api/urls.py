3e8a4d8cf0979aea8f73c77286dcf180fd03de18
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 10:43:40 +0100

    Update urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -1,8 +1,2 @@
-from django.contrib import admin
-from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
-]
 
