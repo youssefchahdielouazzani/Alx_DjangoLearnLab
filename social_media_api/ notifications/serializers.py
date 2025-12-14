e11aac647fac91956db293e2855ac5dfaa7a2303
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 14:28:16 +0100

    Update serializers.py

--- social_media_api/ notifications/serializers.py
+++ social_media_api/ notifications/serializers.py
@@ -1,1 +1,10 @@
+from rest_framework import serializers
+from .models import Notification
+
+class NotificationSerializer(serializers.ModelSerializer):
+    actor_username = serializers.CharField(source='actor.username', read_only=True)
+
+    class Meta:
+        model = Notification
+        fields = ['id', 'recipient', 'actor', 'actor_username', 'verb', 'target', 'timestamp', 'read']
 
