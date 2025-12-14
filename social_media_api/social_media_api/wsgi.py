98a351caf93998714cccc4122d5b46bf1af6cb72
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 13:59:09 +0100

    Update wsgi.py

--- social_media_api/social_media_api/wsgi.py
+++ social_media_api/social_media_api/wsgi.py
@@ -1,8 +1,4 @@
-import os
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
-application = get_wsgi_application()
 
 
 
