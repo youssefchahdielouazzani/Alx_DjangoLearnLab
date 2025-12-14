c10d804e8e5d3b361d4f6b194c1cedc61e8a9477
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-14 15:13:52 +0100

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,20 +1,16 @@
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Post, Like
 from notifications.models import Notification
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
 def like_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
-
     like, created = Like.objects.get_or_create(
         user=request.user,
         post=post
     )
-
     if created:
         Notification.objects.create(
             recipient=post.author,
@@ -22,18 +18,10 @@ def like_post(request, pk):
             verb='liked your post',
             target=post
         )
-        return Response({'message': 'Post liked'})
-
-    return Response({'message': 'Already liked'})
+    return Response({'message': 'liked'})
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
 def unlike_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     Like.objects.filter(user=request.user, post=post).delete()
-    return Response({'message': 'Post unliked'})
-
-
-
-
-
+    return Response({'message': 'unliked'})
