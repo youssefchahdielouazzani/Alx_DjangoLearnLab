0eb0309b7f8545995a0d295d5e95d642d63533d4
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-24 19:50:44 +0100

    Update manage.py

--- django_blog/manage.py
+++ django_blog/manage.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
 
