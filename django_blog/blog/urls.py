448e3c8b73e78a3dfbbd9a44e7d26fcf40dc1574
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-27 20:08:02 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,8 +3,15 @@ from .views import PostDetailView, CommentCreateView, CommentUpdateView, Comment
 
 urlpatterns = [
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
+
+    # Création d'un commentaire
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
+
+    # Modification d'un commentaire
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
+
+    # Suppression d'un commentaire
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
+
 
