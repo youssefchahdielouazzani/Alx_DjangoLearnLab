4da97d46017f2de105ca5fc641763fc08597d1ca
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-27 20:02:15 +0100

    Update admin.py

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,1 +1,15 @@
+from django.contrib import admin
+from .models import Post, Comment
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at', 'author')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at', 'updated_at')
+    search_fields = ('content',)
+    list_filter = ('created_at', 'author')
 
