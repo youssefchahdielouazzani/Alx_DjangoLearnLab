4bf59ea619e6119dbe197e76e13dc5757a69a3ed
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-28 18:40:51 +0100

    Update query_samples.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,1 +1,13 @@
+from .models import Author, Book, Library, Librarian
+
+# All books by a specific author
+author = Author.objects.first()
+books_by_author = Book.objects.filter(author=author)
+
+# All books in a library
+library = Library.objects.first()
+books_in_library = library.books.all()
+
+# Librarian for a library
+librarian = Librarian.objects.get(library=library)
 
