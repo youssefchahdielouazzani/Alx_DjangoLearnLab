683d2556b18224e4103d3a0a5c93f00d0739073e
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-28 09:34:22 +0100

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,1 +1,12 @@
+from django.db import models
+from taggit.managers import TaggableManager
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = TaggableManager(blank=True)
+
+    def __str__(self):
+        return self.title
 
