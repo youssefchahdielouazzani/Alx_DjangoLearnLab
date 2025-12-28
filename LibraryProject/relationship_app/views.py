6c382e7d9805e2f861595612862cf19711414ba4
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-28 17:40:39 +0100

    Update views.py

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -1,9 +1,2 @@
-from django.shortcuts import render
-from .models import Library, Book, Author, Librarian
 
-# Exemple de vue simple affichant toutes les bibliothèques et leurs livres
-def library_list(request):
-    libraries = Library.objects.all()
-    context = {'libraries': libraries}
-    return render(request, 'relationship_app/library_list.html', context)
 
