23b3fb6e92203d7fa00f023b656c99d416c9fedf
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-28 12:05:04 +0100

    Update urls.py

--- LibraryProject/LibraryProject/urls.py
+++ LibraryProject/LibraryProject/urls.py
@@ -1,1 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from relationship_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.library_list, name='library_list'),  # Exemple de vue
+]
 
