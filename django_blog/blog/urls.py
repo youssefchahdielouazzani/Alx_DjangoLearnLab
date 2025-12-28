df0258adb392ebff038b6c42ebee2dfd4983245c
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-28 09:36:57 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,1 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_create, name='post_create'),
+    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
+    path('search/', views.search_posts, name='search'),
+]
+
 
