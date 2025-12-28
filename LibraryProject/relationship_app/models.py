b9f0217354796be282d03539f8e8c6874fd9586c
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-28 17:51:01 +0100

    Update models.py

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -1,1 +1,29 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(
+        Library,
+        related_name="books",
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.title
 
