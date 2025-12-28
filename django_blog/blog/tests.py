021ffd469ab5ad8370ae2258a33e4804672e621e
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-28 09:37:38 +0100

    Update tests.py

--- django_blog/blog/tests.py
+++ django_blog/blog/tests.py
@@ -1,1 +1,11 @@
+from django.test import TestCase
+from .models import Post
+
+class PostModelTest(TestCase):
+    def test_create_post(self):
+        post = Post.objects.create(
+            title='Test Post',
+            content='Test content'
+        )
+        self.assertEqual(post.title, 'Test Post')
 
