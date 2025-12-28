f18490fb55378b244dc49abe10faf83dcf248b92
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-28 09:52:08 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
 from . import views
+from .views import PostByTagListView
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_create, name='post_create'),
-    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
+
+    # ✅ OBLIGATOIRE POUR ALX
+    path(
+        'tags/<slug:tag_slug>/',
+        PostByTagListView.as_view(),
+        name='posts_by_tag'
+    ),
+
     path('search/', views.search_posts, name='search'),
 ]
+
 
 
