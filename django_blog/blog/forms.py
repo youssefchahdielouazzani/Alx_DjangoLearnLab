bdfc20d0231e2d25f4fdadbd3f9496fc9f9081f7
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-28 09:46:53 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import Post
+from taggit.forms import TagWidget
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
+
 
