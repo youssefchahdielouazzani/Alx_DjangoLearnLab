86ca3201864b72c355dfd238288ce1dbbca364d4
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2025-12-31 20:46:50 +0100

    Update query_samples.py

--- LibraryProject/relationship_app/query_samples.py
+++ LibraryProject/relationship_app/query_samples.py
@@ -1,1 +1,18 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query 1: All books by a specific author
+def get_books_by_author(author_name):
+    return Book.objects.filter(author__name=author_name)
+
+
+# Query 2: List all books in a library
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+
+# Query 3: Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian
 
