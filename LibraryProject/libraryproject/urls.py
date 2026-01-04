31d09d3e3c59bf3ddc7f621c69ccc0f2a44864b0
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-04 10:17:21 +0100

    Update urls.py

--- LibraryProject/libraryproject/urls.py
+++ LibraryProject/libraryproject/urls.py
@@ -1,9 +1,9 @@
-# LibraryProject/relationship_app/urls.py
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list'),
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),  # inclut ton urls.py minimal
 ]
 
 
