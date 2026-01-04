e33578a174004aa5c9e5bf21e866dba2f6e03b0d
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-04 10:06:02 +0100

    Update views.py

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -1,1 +1,9 @@
+# LibraryProject/relationship_app/views.py
+from django.shortcuts import render
+from .models import Book
+
+# Vue simple qui liste les livres et leurs auteurs
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
 
