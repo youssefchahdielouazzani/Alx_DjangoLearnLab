feb2cfdcbf2b2bbd15cab549e7433c8cbba199d4
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-04 10:24:17 +0100

    Update views.py

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -1,9 +1,17 @@
 # LibraryProject/relationship_app/views.py
-from django.shortcuts import render
-from .models import Book
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Vue simple qui liste les livres et leurs auteurs
+# Fonction-based view
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'book_list.html', {'books': books})
+
+# Class-based view
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
 
