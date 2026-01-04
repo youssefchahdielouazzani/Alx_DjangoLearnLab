8c6c4a3fa195838507f3c56f45a3e1a5061b57d3
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-04 09:47:19 +0100

    Update models.py

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -1,32 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-
     def __str__(self):
         return self.name
-
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     def __str__(self):
         return self.title
 
-
 class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
-
+    name = models.CharField(max_length=150)
+    books = models.ManyToManyField(Book, related_name='libraries')
     def __str__(self):
         return self.name
 
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
+class Borrow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='borrowed_books')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='borrowings')
+    borrowed_at = models.DateTimeField(auto_now_add=True)
+    returned_at = models.DateTimeField(null=True, blank=True)
     def __str__(self):
-        return self.name
+        return f"{self.user.username} - {self.book.title}"
+
+
 
