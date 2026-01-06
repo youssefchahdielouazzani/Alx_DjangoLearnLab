97dba4e02370d8568bc4dcf84217735822b3a525
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-06 20:37:13 +0100

    Update models.py

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -1,30 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
+
 class Library(models.Model):
-    name = models.CharField(max_length=150)
-    books = models.ManyToManyField(Book, related_name='libraries')
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
+
     def __str__(self):
         return self.name
-
-class Borrow(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='borrowed_books')
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='borrowings')
-    borrowed_at = models.DateTimeField(auto_now_add=True)
-    returned_at = models.DateTimeField(null=True, blank=True)
-    def __str__(self):
-        return f"{self.user.username} - {self.book.title}"
-
-
-
