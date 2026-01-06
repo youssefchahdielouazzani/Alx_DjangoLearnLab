e4b2999c1df9e2e6481fbd278465e109f1051148
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-06 20:25:00 +0100

    Update manage.py

--- LibraryProject/manage.py
+++ LibraryProject/manage.py
@@ -1,15 +1,1 @@
-#!/usr/bin/env python
-import os
-import sys
-
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise
-    execute_from_command_line(sys.argv)
-
-if __name__ == '__main__':
-    main()
 
