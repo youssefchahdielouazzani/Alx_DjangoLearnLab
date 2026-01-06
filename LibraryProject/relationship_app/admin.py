e9d2df8a5596796220d2f9a1cbd21e6524d76be4
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-06 20:39:47 +0100

    Update admin.py

--- LibraryProject/relationship_app/admin.py
+++ LibraryProject/relationship_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Author, Book, Library, Librarian
+from .models import Author, Book, Library
 
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Library)
-admin.site.register(Librarian)
+
 
