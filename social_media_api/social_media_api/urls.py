4707a56e06743497061492fe698a97db5ca33534
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 15:30:07 +0100

    Update urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -1,3 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/users/', include('apps.users.urls')),
+    path('api/posts/', include('apps.posts.urls')),
+]
+
+# Servir les fichiers médias en dev
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
