ab9d538ab5b30aa00acb6fcbfe630b8e49a29b68
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 15:36:01 +0100

    Update serializers.py

--- social_media_api/apps/posts/serializers.py
+++ social_media_api/apps/posts/serializers.py
@@ -1,1 +1,11 @@
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'author_username', 'content', 'created_at']
+        read_only_fields = ['id', 'author_username']
 
