cf633c8aa1458a3ad35fe53717e3267815721633
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 11:26:19 +0100

    Remove URL patterns from urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -1,9 +1,2 @@
-from django.contrib import admin
-from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls')),
-    path('notifications/', include('notifications.urls')),
-]
 
