55ba7f0068afb8617f6cb43de080fc1f5f872bbe
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 12:45:50 +0100

    Add authenticated feed view for user posts

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,1 +1,17 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def feed(request):
+    followed_users = request.user.following.all()
+    posts = Post.objects.filter(
+        author__in=followed_users
+    ).order_by('-created_at')
+
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
