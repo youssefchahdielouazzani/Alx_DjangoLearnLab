246bb1f94800e0ab0c3612864bb2c5ae612699d8
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 12:45:11 +0100

    Update serializers.py

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,1 +1,8 @@
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'content', 'created_at']
 
