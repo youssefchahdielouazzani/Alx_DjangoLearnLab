295ebe3b04835335688e255240e6ebd1486b3aba
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 16:06:20 +0100

    Update views.py

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -1,19 +1,3 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book, Library
-
-# Function-Based View
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {
-        'books': books
-    })
-
-# Class-Based View
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
 
 
 
