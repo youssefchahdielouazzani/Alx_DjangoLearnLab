a5227059ffecbc7b1f282ee72659f323118f8128
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 14:38:24 +0100

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,10 +1,4 @@
-from django.urls import path
-from .views import FollowUserView, UnfollowUserView
 
-urlpatterns = [
-    path('follow/<int:user_id>/', FollowUserView.as_view()),
-    path('unfollow/<int:user_id>/', UnfollowUserView.as_view()),
-]
 
 
 
