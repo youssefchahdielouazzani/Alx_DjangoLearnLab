fd959efb8d810784c9291d9e300a5751577c1572
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 14:37:01 +0100

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,12 +1,3 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
 
-class User(AbstractUser):
-    following = models.ManyToManyField(
-        'self',
-        symmetrical=False,
-        related_name='followers',
-        blank=True
-    )
 
 
