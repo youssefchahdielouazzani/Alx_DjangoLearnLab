22c79c8d54af7622b5f9e7f0485b3fa5b6ef59d6
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 11:33:16 +0100

    Extend User model with bio and profile picture

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,1 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.URLField(blank=True)
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
 
