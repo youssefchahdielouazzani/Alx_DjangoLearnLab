0243598647d75561db6448771e0c08b4220cf59e
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 15:33:29 +0100

    Update models.py

--- social_media_api/apps/users/models.py
+++ social_media_api/apps/users/models.py
@@ -1,1 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    # Exemple : ajouter un champ bio
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
 
