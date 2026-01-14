51c13f9eec2f2bb1106fdf1c7666d0685e14ca2d
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 11:48:44 +0100

    Refactor token retrieval in RegisterView

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,18 +3,15 @@ from rest_framework.response import Response
 from rest_framework import status, permissions
 from rest_framework.authtoken.models import Token
 
-from .serializers import (
-    RegisterSerializer,
-    LoginSerializer,
-    UserProfileSerializer
-)
+from .serializers import RegisterSerializer, LoginSerializer, UserProfileSerializer
+
 
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            token, _ = Token.objects.get_or_create(user=user)
+            token = Token.objects.get(user=user)
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -35,4 +32,3 @@ class ProfileView(APIView):
     def get(self, request):
         serializer = UserProfileSerializer(request.user)
         return Response(serializer.data)
-
