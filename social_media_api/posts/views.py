388a2012aa66b5ec69f5e478a474e73645bb8f35
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 13:58:55 +0100

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,16 +1,5 @@
-from rest_framework import generics, permissions
-from rest_framework.response import Response
-from .models import Post
-from .serializers import PostSerializer
+Post.objects.filter(author__in=following_users).order_by(...)
 
-class FeedView(generics.GenericAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request):
-        followed_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
 
 
 
