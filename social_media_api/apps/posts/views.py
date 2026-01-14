c3593b195adaa674542cf6b3f1852978959c5cf2
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 15:36:35 +0100

    Update views.py

--- social_media_api/apps/posts/views.py
+++ social_media_api/apps/posts/views.py
@@ -1,1 +1,20 @@
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+# Liste tous les posts et créer un post
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+# Détails, mise à jour et suppression d’un post
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
