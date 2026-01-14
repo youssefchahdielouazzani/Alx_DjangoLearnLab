30a928f6530224ca33986a587c16c8fb7876bbbc
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 15:34:40 +0100

    Update views.py

--- social_media_api/apps/users/views.py
+++ social_media_api/apps/users/views.py
@@ -1,1 +1,17 @@
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+
+# Liste tous les utilisateurs (lecture seule)
+class UserListView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+# Détails d’un utilisateur
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
