49d8fb57d453aba5b76a887ddd8bcd3f19a5f19d
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 12:34:14 +0100

    Clean up asgi.py by removing unused code
    Remove unnecessary imports and environment setup.

--- social_media_api/social_media_api/asgi.py
+++ social_media_api/social_media_api/asgi.py
@@ -1,8 +1,3 @@
-import os
-from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
-
-application = get_asgi_application()
 
 
