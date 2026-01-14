8f2ab76899cacfb8b879a4a64833a98ccde5170c
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 11:30:35 +0100

    Set up WSGI for Django application

--- social_media_api/social_media_api/wsgi.py
+++ social_media_api/social_media_api/wsgi.py
@@ -1,2 +1,8 @@
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
+
+application = get_wsgi_application()
 
 
