6f48c8474a56d6567660e71addea5c3b24ed953a
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 12:42:51 +0100

    Add follow and unfollow user endpoints

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,1 +1,20 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import User
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(User, id=user_id)
+    request.user.following.add(user_to_follow)
+    return Response({"message": "User followed successfully"})
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(User, id=user_id)
+    request.user.following.remove(user_to_unfollow)
+    return Response({"message": "User unfollowed successfully"})
 
