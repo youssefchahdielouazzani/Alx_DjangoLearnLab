3087c32df459e74cda373d8ae3872d90354d87ac
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 15:42:18 +0100

    Update gunicorn_config.py

--- social_media_api/gunicorn_config.py
+++ social_media_api/gunicorn_config.py
@@ -1,1 +1,6 @@
+bind = "0.0.0.0:8000"
+workers = 3  # nombre de workers pour gérer les requêtes simultanées
+accesslog = "-"  # logs d'accès
+errorlog = "-"   # logs d'erreurs
+timeout = 120    # temps maximal de requête
 
