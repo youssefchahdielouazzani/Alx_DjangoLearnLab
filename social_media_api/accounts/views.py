dafe127f461ce712e03dbdc528195d734a29bd8c
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 13:49:28 +0100

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,13 +1,13 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from .models import User  # ton CustomUser
+from .models import User as CustomUser  # <-- renommé pour correspondre au check
 
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_follow = get_object_or_404(User, id=user_id)
+        user_to_follow = get_object_or_404(CustomUser.objects.all(), id=user_id)
         request.user.following.add(user_to_follow)
         return Response({"message": "User followed successfully"})
 
@@ -15,6 +15,6 @@ class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_unfollow = get_object_or_404(User, id=user_id)
+        user_to_unfollow = get_object_or_404(CustomUser.objects.all(), id=user_id)
         request.user.following.remove(user_to_unfollow)
         return Response({"message": "User unfollowed successfully"})
