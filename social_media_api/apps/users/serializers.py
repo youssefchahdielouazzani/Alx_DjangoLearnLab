3cb64f72c7ae787eba02116d89316af547a59a95
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 15:34:07 +0100

    Update serializers.py

--- social_media_api/apps/users/serializers.py
+++ social_media_api/apps/users/serializers.py
@@ -1,1 +1,9 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'bio']
+        read_only_fields = ['id']
 
