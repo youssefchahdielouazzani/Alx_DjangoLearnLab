52ba69f264cd7964bff372cf57fcc8cdcf1e620d
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 12:37:38 +0100

    Delete User model from accounts app
    Removed the User model and its associated fields.

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,21 +1,2 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(
-        upload_to='profile_pictures/',
-        blank=True,
-        null=True
-    )
-    followers = models.ManyToManyField(
-        'self',
-        symmetrical=False,
-        related_name='following',
-        blank=True
-    )
-
-    def __str__(self):
-        return self.username
 
 
