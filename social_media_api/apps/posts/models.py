d552e8e7c7b81fac0b45d723e8aba54add5baa3b
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 15:35:25 +0100

    Update models.py

--- social_media_api/apps/posts/models.py
+++ social_media_api/apps/posts/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+from apps.users.models import CustomUser
+
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.id} by {self.author.username}"
 
