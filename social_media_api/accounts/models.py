1ec10846f5b6671f8220a0c639322d5a7bd9404b
youssefchahdielouazzani <youssefchahdielouazzani@gmail.com>
2026-01-14 11:43:35 +0100

    Change profile picture field to ImageField

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 class User(AbstractUser):
     bio = models.TextField(blank=True)
-    profile_picture = models.URLField(blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/',
+        blank=True,
+        null=True
+    )
     followers = models.ManyToManyField(
         'self',
         symmetrical=False,
@@ -13,4 +17,5 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
 
